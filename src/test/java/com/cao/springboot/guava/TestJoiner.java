e9abdaac06be5df9ee83df4b15df0042510a973f
ccwwh <ccwwhh1993@163.com>
2019-03-19 14:14:10 +0800

    测试Guava

--- src/test/java/com/cao/springboot/guava/TestJoiner.java
+++ src/test/java/com/cao/springboot/guava/TestJoiner.java
@@ -1,9 +1,14 @@
 package com.cao.springboot.guava;
 
+import org.junit.Test;
+
 /**
  * @author cwh
  * @date 2019/3/19
  */
 public class TestJoiner {
+    @Test
+    public void test1(){
 
+    }
 }
